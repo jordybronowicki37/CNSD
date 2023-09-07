7e258a5628015efe613cababd5ff08c7fce7482a
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-07 09:05:21 +0200

    Changed identifier lists in dto's to use the id's

--- module-1/opdracht_1_4/src/main/java/com/jb_cnsd/opdracht_1_2/web/controller/dto/PersoonDto.java
+++ module-1/opdracht_1_4/src/main/java/com/jb_cnsd/opdracht_1_2/web/controller/dto/PersoonDto.java
@@ -5,13 +5,13 @@ import com.jb_cnsd.opdracht_1_2.data.models.Persoon;
 
 import java.util.List;
 
-public record PersoonDto(long id, String bsn, String naam, List<String> rekeningen) {
+public record PersoonDto(long id, String bsn, String naam, List<Long> rekeningen) {
     public PersoonDto(Persoon persoon) {
         this(
                 persoon.getId(),
                 persoon.getBsn(),
                 persoon.getNaam(),
-                persoon.getRekeningen().stream().map(Rekening::getIban).toList()
+                persoon.getRekeningen().stream().map(Rekening::getId).toList()
         );
     }
 }
--- module-1/opdracht_1_4/src/main/java/com/jb_cnsd/opdracht_1_2/web/controller/dto/RekeningDto.java
+++ module-1/opdracht_1_4/src/main/java/com/jb_cnsd/opdracht_1_2/web/controller/dto/RekeningDto.java
@@ -6,14 +6,14 @@ import com.jb_cnsd.opdracht_1_2.data.models.RekeningStatus;
 
 import java.util.List;
 
-public record RekeningDto(long id, String iban, float saldo, RekeningStatus status, List<String> rekeningHouders) {
+public record RekeningDto(long id, String iban, float saldo, RekeningStatus status, List<Long> rekeningHouders) {
     public RekeningDto(Rekening rekening) {
         this(
                 rekening.getId(),
                 rekening.getIban(),
                 rekening.getSaldo(),
                 rekening.getStatus(),
-                rekening.getPersonen().stream().map(Persoon::getBsn).toList()
+                rekening.getPersonen().stream().map(Persoon::getId).toList()
         );
     }
 }
