673f7323e74fb4b6e71ce79cc8bd29a2603b7ad9
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-10 11:42:21 +0200

    Changed directory name

--- module-1/opdracht_1_4/src/main/java/com/jb_cnsd/opdracht_1_2/web/dto/requests/RekeningEditRequest.java
+++
@@ -1,6 +0,0 @@
-package com.jb_cnsd.opdracht_1_2.web.dto.requests;
-
-import com.jb_cnsd.opdracht_1_2.data.models.RekeningStatus;
-
-public record RekeningEditRequest(RekeningStatus status) {
-}
---
+++ module-1/opdracht_1_4/src/main/java/com/jb_cnsd/opdracht_1_4/data/repository/RekeningRepository.java
@@ -0,0 +1,10 @@
+package com.jb_cnsd.opdracht_1_4.data.repository;
+
+import com.jb_cnsd.opdracht_1_4.data.models.Rekening;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RekeningRepository extends JpaRepository<Rekening, Long> {
+    boolean existsByIban(String iban);
+}
