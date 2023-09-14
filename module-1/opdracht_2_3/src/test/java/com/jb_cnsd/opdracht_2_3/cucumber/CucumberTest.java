3564bf7c8c7e9bb1ebc4cc7b3321b18f12f6def2
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-14 15:31:13 +0200

    Added cucumber tests for saldo

---
+++ module-1/opdracht_2_3/src/test/java/com/jb_cnsd/opdracht_2_3/cucumber/CucumberTest.java
@@ -0,0 +1,12 @@
+package com.jb_cnsd.opdracht_2_3.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features")
+public class CucumberTest {
+
+}
+
