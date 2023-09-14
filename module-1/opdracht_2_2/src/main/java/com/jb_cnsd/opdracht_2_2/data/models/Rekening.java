cf8dc759b97891fa5bebe147e3733d72a0ccd885
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-14 09:34:58 +0200

    Changed rekening personen list to fetch with eager strategy

--- module-1/opdracht_2_2/src/main/java/com/jb_cnsd/opdracht_2_2/data/models/Rekening.java
+++ module-1/opdracht_2_2/src/main/java/com/jb_cnsd/opdracht_2_2/data/models/Rekening.java
@@ -22,7 +22,7 @@ public class Rekening extends BaseEntity {
     @Column(length = 16)
     private RekeningStatus status;
 
-    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
     @JoinTable(
             name = "_persoon_rekening",
             joinColumns = @JoinColumn(name = "rekening_id"),
