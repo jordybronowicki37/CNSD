57833068203b5aabec88142763aab9a89ea87993
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-30 21:05:57 +0200

    Added react template

---
+++ module-1/opdracht_5_1/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Header} from "./components/Header.tsx";
+
+function App() {
+
+  return (
+    <div>
+        <Header/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ module-1/opdracht_5_1/frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+export function Header() {
+    return (
+        <header>
+            <h1>CNSD Account</h1>
+            <nav>
+                <a href="">login</a>
+            </nav>
+        </header>
+    );
+}
