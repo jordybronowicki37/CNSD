8c6b6204ee43c64228c0455fb1d6e809a9822a2b
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-03 21:04:03 +0200

    Added create account feature

--- module-1/opdracht_5_2/frontend/src/data/requesters/AccountRequester.ts
+++ module-1/opdracht_5_2/frontend/src/data/requesters/AccountRequester.ts
@@ -1,5 +1,5 @@
 import {Store} from "../DataStore.ts";
-import {accountsSetAction} from "../reducers/AccountReducer.ts";
+import {accountAddAction, accountsSetAction} from "../reducers/AccountReducer.ts";
 import {Account} from "../Types.ts";
 
 export async function getAccounts() {
@@ -10,4 +10,18 @@ export async function getAccounts() {
   if (!response.ok) throw new Error("Accounts fetch failed");
   const json = await response.json() as Account[];
   Store.dispatch(accountsSetAction(json));
+}
+
+export async function createAccount() {
+  const user = Store.getState().user;
+  if (user === null) return;
+
+  const response = await fetch(`/api/rekening`, {
+    body: JSON.stringify({persoonId: user.id}),
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+  });
+  if (!response.ok) throw new Error("Account create failed");
+  const json = await response.json() as Account;
+  Store.dispatch(accountAddAction(json));
 }
--- module-1/opdracht_5_2/frontend/src/pages/OverviewPage.tsx
+++ module-1/opdracht_5_2/frontend/src/pages/OverviewPage.tsx
@@ -4,23 +4,37 @@ import {useSelector} from "react-redux";
 import {StoreTypes} from "../data/DataStore.ts";
 import {Holder} from "../data/Types.ts";
 import {useEffect, useState} from "react";
-import {getAccounts} from "../data/requesters/AccountRequester.ts";
+import {createAccount, getAccounts} from "../data/requesters/AccountRequester.ts";
 import {getHolders} from "../data/requesters/HolderRequester.ts";
 import {useCheckForUserNotLoggedIn} from "../hooks/CheckLoginHook.ts";
 
 export function OverviewPage() {
   useCheckForUserNotLoggedIn();
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [newAccountLoading, setNewAccountLoading] = useState(false);
   const accounts = useSelector<StoreTypes, StoreTypes["accounts"]>(s => s.accounts);
   const holders = useSelector<StoreTypes, StoreTypes["holders"]>(s => s.holders);
 
   useEffect(() => {
     const holdersPromise = getHolders();
     const accountsPromise = getAccounts();
-    Promise.all([holdersPromise, accountsPromise]).then(_ => {
-      setLoading(false);
-    })
+    Promise.all([holdersPromise, accountsPromise])
+      .then(_ => {
+        setLoading(false);
+      })
+      .catch(_ => {
+        setLoading(false);
+        setError(true);
+      });
   }, []);
+
+  const onCreateAccount = () => {
+    setNewAccountLoading(true);
+    createAccount().then(_ => {
+      setNewAccountLoading(false)
+    });
+  }
 
   return (
     <div className="overview-page">
@@ -35,8 +49,18 @@ export function OverviewPage() {
         <div hidden={!loading}>
           Loading...
         </div>
-        <div className="accounts-container" hidden={loading}>
+        <div hidden={!error}>
+          Something went wrong!
+        </div>
+        <div className="accounts-container" hidden={loading || error}>
           {accounts.map(v => <AccountItem key={v.id} account={v} title={findHolderName(v.personen[0], holders)}/>)}
+        </div>
+        <div>
+          <button
+            disabled={newAccountLoading}
+            onClick={onCreateAccount}
+            className="create-new-account"
+            hidden={loading || error}>Create new account</button>
         </div>
       </div>
     </div>
