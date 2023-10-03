d82c5bb3e75220cb94de374bafec282883f771e5
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-03 14:41:39 +0200

    Changed proxy to use the vite config

--- module-1/opdracht_5_2/frontend/vite.config.ts
+++ module-1/opdracht_5_2/frontend/vite.config.ts
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({mode}) => {
+  let target = "http://localhost:8080";
+  if (mode === "production") {
+    // TODO add aws api uri
+    target = "http://localhost:8081"
+  }
+
+  const config = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
+  return defineConfig(config);
+}
