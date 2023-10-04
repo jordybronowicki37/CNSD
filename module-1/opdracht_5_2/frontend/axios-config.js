9bc43a70f798b8539c9c292991da515f56f20c02
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-04 20:51:03 +0200

    Removed prod alb uri

--- module-1/opdracht_5_2/frontend/axios-config.js
+++ module-1/opdracht_5_2/frontend/axios-config.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const prod = import.meta.env.PROD;
-if (prod) axios.defaults.baseURL = "http://ecs-cluster-staging-alb-1837777129.us-east-1.elb.amazonaws.com";
-else axios.defaults.baseURL = "http://localhost:8080";
+const dev = import.meta.env.DEV;
+if (dev) axios.defaults.baseURL = "http://localhost:8080";
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
