03eb8ece8b1eeb9eb9080652c44f57af96bc4fd2
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-04 19:48:37 +0200

    Fixed signup api call and added error message on failure

--- module-1/opdracht_5_2/frontend/src/pages/SignupPage.tsx
+++ module-1/opdracht_5_2/frontend/src/pages/SignupPage.tsx
@@ -2,9 +2,12 @@ import "./SignupPage.scss";
 import {useHistory} from "react-router-dom";
 import {ChangeEvent, FormEvent, useState} from "react";
 import {useCheckForUserAlreadyLoggedIn} from "../hooks/CheckLoginHook.ts";
+import {signupUser} from "../data/requesters/UserRequester.ts";
 
 export function SignupPage() {
   useCheckForUserAlreadyLoggedIn();
+  const [loading, setLoading] = useState(false);
+  const [signupFailed, setSignupFailed] = useState(false);
   const history = useHistory();
 
   const [formData, setFormData] = useState({
@@ -21,7 +24,13 @@ export function SignupPage() {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    history.push("/overview");
+    setLoading(true);
+    signupUser(formData.username, formData.password)
+      .then(() => history.push("/overview"))
+      .catch(() => {
+        setLoading(false);
+        setSignupFailed(true);
+      });
   }
 
   return (
@@ -29,28 +38,60 @@ export function SignupPage() {
       <div className="signup-card">
         <h2>Signup</h2>
         <p>Signup to continue</p>
+        <p
+          className="signup-failed-message"
+          hidden={!signupFailed}
+          >Signup failed, try again later.</p>
 
         <form onSubmit={handleSubmit}>
           <div className="input-container">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" value={formData.username} onChange={handleInputChange}/>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              disabled={loading}
+              onChange={handleInputChange}/>
           </div>
 
           <div className="input-container">
             <label htmlFor="iban">Iban</label>
-            <input type="text" id="iban" name="iban" minLength={9} maxLength={9} value={formData.iban} onChange={handleInputChange}/>
+            <input
+              type="text"
+              id="iban"
+              name="iban"
+              minLength={9}
+              maxLength={9}
+              value={formData.iban}
+              disabled={loading}
+              onChange={handleInputChange}/>
           </div>
 
           <div className="input-container">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" value={formData.password} onChange={handleInputChange}/>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              disabled={loading}
+              onChange={handleInputChange}/>
           </div>
 
           <div className="input-container">
             <label htmlFor="password2">Repeat password</label>
-            <input type="password" id="password2" name="password2" value={formData.password2} onChange={handleInputChange}/>
+            <input
+              type="password"
+              id="password2"
+              name="password2"
+              value={formData.password2}
+              disabled={loading}
+              onChange={handleInputChange}/>
           </div>
-          <button type="submit">Signup</button>
+          <button
+            disabled={loading}
+            type="submit">Signup</button>
         </form>
       </div>
     </div>
