b749e6123ba70dff52addccb75d3677cc2413d09
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-04 17:45:54 +0200

    Changed default url to alb

--- module-1/opdracht_5_2/frontend/axios-config.js
+++ module-1/opdracht_5_2/frontend/axios-config.js
@@ -1,5 +1,4 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://ecs-cluster-staging-alb-1837777129.us-east-1.elb.amazonaws.com";
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "http://ecs-cluster-staging-alb-1837777129.us-east-1.elb.amazonaws.com";
 axios.defaults.headers.post['Content-Type'] = 'application/json';
