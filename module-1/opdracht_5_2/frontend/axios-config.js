ceccebba07d4e0ba82c70833bedcf0ad5a5c7911
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-04 19:57:55 +0200

    Fixed local dev environment still connecting to alb

--- module-1/opdracht_5_2/frontend/axios-config.js
+++ module-1/opdracht_5_2/frontend/axios-config.js
@@ -1,4 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://ecs-cluster-staging-alb-1837777129.us-east-1.elb.amazonaws.com";
+const prod = import.meta.env.PROD;
+if (prod) axios.defaults.baseURL = "http://ecs-cluster-staging-alb-1837777129.us-east-1.elb.amazonaws.com";
+else axios.defaults.baseURL = "http://localhost:8080";
+
 axios.defaults.headers.post['Content-Type'] = 'application/json';
