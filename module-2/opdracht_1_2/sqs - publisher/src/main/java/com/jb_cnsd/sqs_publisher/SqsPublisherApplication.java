c42a122faf077f432c826cc8fe38348661d5b608
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-24 11:55:33 +0200

    Added sql publisher project

---
+++ module-2/opdracht_1_2/sqs - publisher/src/main/java/com/jb_cnsd/sqs_publisher/SqsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.jb_cnsd.sqs_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqsPublisherApplication.class, args);
+    }
+
+}
---
+++ module-2/opdracht_1_2/sqs - publisher/src/test/java/com/jb_cnsd/sqs_publisher/SqsPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb_cnsd.sqs_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
