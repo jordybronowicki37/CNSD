f1f6785911eae0bb9e4d4cb8c6f110d1a8875bd4
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-11-07 13:21:51 +0100

    Added xray and ssm in opdracht_2_3

--- module-2/opdracht_2_3/sam-app/photo_processor/app.py
+++ module-2/opdracht_2_3/sam-app/photo_processor/app.py
@@ -1,45 +1,51 @@
 from __future__ import print_function
-import os
 import boto3
 import json
 import base64
+from aws_xray_sdk.core import patch_all
 
-xrayPatched = False
+# Enable XRay tracing on client libraries
+patch_all()
+
+# Initialize the client
+s3 = boto3.resource('s3')
+sns = boto3.client('sns')
+ssm = boto3.client('ssm')
+
+# Read the configuration properties
+destBucketname = ssm.get_parameter(Name="/dev/photoProcessor/destBucketname")['Parameter']['Value']
+print(f"{destBucketname =}")
+
+snsTopicArn = ssm.get_parameter(Name="/dev/photoProcessor/snsTopicArn")['Parameter']['Value']
+print(f"{snsTopicArn =}")
 
 
 def lambda_handler(event, context):
-    print("Deployed automatically with Jenkins")
-    s3 = boto3.resource('s3')
-    sns = boto3.client('sns')
-
-    # Read the configuration properties
-    destBucketname = os.environ['DESTINATION_S3_BUCKETNAME']
-    print(f"{destBucketname =}")
-
-    snsTopicArn = os.environ['SNS_TOPIC_ARN']
-    print(f"{snsTopicArn =}")
-
     # Process each message
     for record in event['Records']:
         # Read the image message from the record (extended message)
-        print(f"{record} {type(record)}")
+        print(f"{record =} {type(record)}")
+
         body = record["body"]
-        print(f"{body} {type(body)}")
-        extmessage = json.loads(body)
-        print(f"{extmessage} {type(extmessage)}")
-        bucketname = extmessage[1]["s3BucketName"]
-        itemname = extmessage[1]["s3Key"]
-        print(f"Extended message body at S3 {bucketname}:{itemname}")
-        srcObj = s3.Object(bucketname, itemname)
+        print(f"{body =} {type(body)}")
+
+        message = json.loads(body)
+        print(f"{message =} {type(message)}")
+
+        bucketName = message[1]["s3BucketName"]
+        itemName = message[1]["s3Key"]
+        print(f"Extended message body at S3 {bucketName}:{itemName}")
+
+        srcObj = s3.Object(bucketName, itemName)
         base64_img = srcObj.get()['Body'].read()
         decoded_image_data = base64.decodebytes(base64_img)
         print(f"Retreived and decoded the message body")
 
         # Save the image to the Photo Processor S3 bucket
-        destItemname = itemname + ".jpg"
-        destObj = s3.Object(destBucketname, destItemname)
+        destItemName = itemName + ".jpg"
+        destObj = s3.Object(destBucketname, destItemName)
         destObj.put(Body=decoded_image_data, ACL='public-read')
-        objectUrl = f"https://{destBucketname}.s3.amazonaws.com/{destItemname}";
+        objectUrl = f"https://{destBucketname}.s3.amazonaws.com/{destItemName}"
         print(f"Image processed and stored at {objectUrl}")
 
         # Send notification to email subscribers
@@ -49,4 +55,4 @@ def lambda_handler(event, context):
 
         # Delete the S3 content of the extended message
         srcObj.delete()
-        print(f"Extended message body deleted {bucketname}:{itemname}")
+        print(f"Extended message body deleted {bucketName}:{itemName}")
