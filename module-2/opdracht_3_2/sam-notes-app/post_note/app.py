7ef39496a9a7199e6e54ef4c10647d7a9923af2e
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-11-08 16:44:31 +0100

    Finished get all and post on 2-3-2

--- module-2/opdracht_3_2/sam-notes-app/post_note/app.py
+++ module-2/opdracht_3_2/sam-notes-app/post_note/app.py
@@ -1,50 +1,31 @@
-import datetime
-import decimal
 import json
 import boto3
+import uuid
 from aws_xray_sdk.core import patch_all
-from sqlalchemy import text, create_engine
 
 patch_all()
-
-ssm = boto3.client('ssm')
-username = ssm.get_parameter(Name="/dev/database/notes/admin/username", WithDecryption=True)['Parameter']['Value']
-password = ssm.get_parameter(Name="/dev/database/notes/admin/password", WithDecryption=True)['Parameter']['Value']
-database_url = ssm.get_parameter(Name="/dev/database/notes/url", WithDecryption=True)['Parameter']['Value']
-database_name = ssm.get_parameter(Name="/dev/database/notes/name")['Parameter']['Value']
-port = 5432
-
-connection_string = f"postgresql://{username}:{password}@{database_url}:{port}/{database_name}"
-
-engine = create_engine(connection_string)
-
-
-def alchemy_encoder(obj):
-    """JSON encoder function for SQLAlchemy special classes."""
-    if isinstance(obj, datetime.date):
-        return obj.isoformat()
-    elif isinstance(obj, decimal.Decimal):
-        return float(obj)
+dynamodb = boto3.resource('dynamodb')
 
 
 def lambda_handler(event, context):
     print(f"{event =} {type(event)}")
 
-    note_text = json.loads(event.get('body')).get('text')
+    user_id = event['pathParameters']['user_id']
+    body = json.loads(event["body"])
 
-    with engine.connect() as connection:
-        query = text(f"INSERT INTO public.notes (text) VALUES (:x) RETURNING *")
-        query = query.bindparams(x=note_text)
-        result = connection.execute(query)
+    table = dynamodb.Table('notes')
 
-    print(f"{result =}")
-    notes = json.dumps([dict(row) for row in result], default=alchemy_encoder)
+    item = {
+        'PK': f'USER#{user_id}',
+        'SK': f'NOTE#{str(uuid.uuid4())}',
+        'text': body["text"],
+        'Type': 'NOTE'
+    }
+
+    response = table.put_item(Item=item)
+    print(f"{response =} {type(response)}")
 
     return {
-        'statusCode': 200,
-        'body': notes,
-        'headers': {
-            'content-type': 'application/json'
-        },
-        "isBase64Encoded": False
+        "statusCode": 200,
+        "body": json.dumps(item)
     }
