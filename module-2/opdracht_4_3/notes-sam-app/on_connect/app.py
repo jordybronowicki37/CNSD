965e4bc431e448956a5d56024fa1312aca2a6b8f
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-11-19 19:53:10 +0100

    Added websocket connection lambda's to 2-4-3

---
+++ module-2/opdracht_4_3/notes-sam-app/on_connect/app.py
@@ -0,0 +1,27 @@
+from os import environ
+import boto3
+from botocore.exceptions import ClientError
+from aws_xray_sdk.core import patch_all
+
+patch_all()
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(environ['CONNECTIONS_TABLE_NAME'])
+
+
+def lambda_handler(event, context):
+    print(f"{event = }")
+    connectionId = event['requestContext']['connectionId']
+
+    try:
+        table.put_item(Item={'connectionId': connectionId})
+    except ClientError as error:
+        print(f"{error = }")
+        return {
+            'statusCode': 500,
+            'body': 'Failed to connect!'
+        }
+
+    return {
+        'statusCode': 200,
+        'body': 'Connected.'
+    }
