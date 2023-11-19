3bd3a358a28d7d39c6cfb4a6e2f373ee118cc5d4
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-11-19 20:51:50 +0100

    Added notification triggers to all the modifiers

--- module-2/opdracht_4_3/notes-sam-app/delete_note/app.py
+++ module-2/opdracht_4_3/notes-sam-app/delete_note/app.py
@@ -1,3 +1,4 @@
+import json
 from os import environ
 import boto3
 from aws_xray_sdk.core import patch_all
@@ -5,6 +6,37 @@ from aws_xray_sdk.core import patch_all
 patch_all()
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(environ['NOTES_TABLE_NAME'])
+connectionsTable = dynamodb.Table(environ['CONNECTIONS_TABLE_NAME'])
+
+
+def notify_users(event, item):
+    try:
+        response = table.scan()
+        allConnections = response["Items"]
+        domain_name = event['requestContext']['domainName']
+        stage = event['requestContext']['stage']
+        gateway_management_api = boto3.client('apigatewaymanagementapi', endpoint_url=f"https://{domain_name}/{stage}")
+        eventData = json.dumps({
+            "action": "deleted_note",
+            "data": item
+        })
+
+        for c in allConnections:
+            connectionId = c["connectionId"]
+            try:
+                gateway_management_api.post_to_connection(
+                    Data=eventData,
+                    ConnectionId=connectionId
+                )
+            except Exception as error:
+                if type(error).__name__ == "GoneException":
+                    print(f"Found stale connection, deleting {connectionId}")
+                    table.delete_item(Key={'connectionId': connectionId})
+                else:
+                    print(f"Error sending message to websocket: {error}")
+
+    except Exception as error:
+        print(f"An error occurred during notifying. {error = }")
 
 
 def lambda_handler(event, context):
@@ -12,14 +44,17 @@ def lambda_handler(event, context):
     user_id = event['pathParameters']['user_id']
     note_id = event['pathParameters']['note_id']
 
+    item = {
+        'PK': f'USER#{user_id}',
+        'SK': f'NOTE#{note_id}'
+    }
     response = table.delete_item(
-        Key={
-            'PK': f'USER#{user_id}',
-            'SK': f'NOTE#{note_id}'
-        },
+        Key=item,
         ReturnValues='ALL_OLD'
     )
     print(f"{response = }")
+
+    notify_users(event, item)
 
     if 'Attributes' not in response:
         return {
--- module-2/opdracht_4_3/notes-sam-app/put_note/app.py
+++ module-2/opdracht_4_3/notes-sam-app/put_note/app.py
@@ -7,6 +7,37 @@ from aws_xray_sdk.core import patch_all
 patch_all()
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(environ['NOTES_TABLE_NAME'])
+connectionsTable = dynamodb.Table(environ['CONNECTIONS_TABLE_NAME'])
+
+
+def notify_users(event, item):
+    try:
+        response = table.scan()
+        allConnections = response["Items"]
+        domain_name = event['requestContext']['domainName']
+        stage = event['requestContext']['stage']
+        gateway_management_api = boto3.client('apigatewaymanagementapi', endpoint_url=f"https://{domain_name}/{stage}")
+        eventData = json.dumps({
+            "action": "changed_note",
+            "data": item
+        })
+
+        for c in allConnections:
+            connectionId = c["connectionId"]
+            try:
+                gateway_management_api.post_to_connection(
+                    Data=eventData,
+                    ConnectionId=connectionId
+                )
+            except Exception as error:
+                if type(error).__name__ == "GoneException":
+                    print(f"Found stale connection, deleting {connectionId}")
+                    table.delete_item(Key={'connectionId': connectionId})
+                else:
+                    print(f"Error sending message to websocket: {error}")
+
+    except Exception as error:
+        print(f"An error occurred during notifying. {error = }")
 
 
 def lambda_handler(event, context):
@@ -33,6 +64,9 @@ def lambda_handler(event, context):
             ReturnValues="ALL_NEW"
         )
         print(f"{response = }")
+
+        notify_users(event, response['Attributes'])
+
         return {
             'statusCode': 200,
             'body': json.dumps(response['Attributes']),
