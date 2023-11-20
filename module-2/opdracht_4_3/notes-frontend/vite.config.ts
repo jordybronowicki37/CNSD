c8e216fb9bcf6db509fe51bf629bfcea1724bda3
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-11-20 13:36:58 +0100

    Added proxy for api calls

--- module-2/opdracht_4_3/notes-frontend/vite.config.ts
+++ module-2/opdracht_4_3/notes-frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/users': {
+          target: env.VITE_API_URI,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+  }
 })
